bb7cb19a1f43dc01ab51ce406a02d843b214c441
Kaab-Bhinder <fa23-bcs-068@isbstudent.comsats.edu.pk>
2025-07-18 01:23:42 +0500

    changes

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth ">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-dartTheme
         dark:text-white`}
